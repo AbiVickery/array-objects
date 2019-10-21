230a56f1a175017d631644dc65e4d8c732883d57
Abi Vickery <vickeryabi@gmail.com>
2019-10-21 13:37:55 -0500

    arrays

--- javascript/app.js
+++ javascript/app.js
@@ -4,3 +4,5 @@ Arrays and Objects
 // Arrays pg 70
 var arr0 = new Array('Hello','World', '!'); // array constructor
 var arr1 = ['Hello','World', '!']; // array literal
+
+console.log(arr1[1]); // World
