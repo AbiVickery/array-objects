c5c2e62e88ef5557bcea772b3569ea20651614e5
Abi Vickery <vickeryabi@gmail.com>
2019-10-21 13:43:23 -0500

    hello world

--- javascript/app.js
+++ javascript/app.js
@@ -4,5 +4,10 @@ Arrays and Objects
 // Arrays pg 70
 var arr0 = new Array('Hello','World', '!'); // array constructor
 var arr1 = ['Hello','World', '!']; // array literal
+var arr2 = [
+    ['Hello','World', '!']
+    ['Nǐ hǎo','shìjiè', '!']
+    ['Hola','Mundo', '!']
+]
 
 console.log(arr1[1]); // World
