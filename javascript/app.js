ff490cbf4b5310d917191d2a6cbcab72e79dc815
Abi Vickery <vickeryabi@gmail.com>
2019-10-23 10:47:53 -0500

    something

--- javascript/app.js
+++ javascript/app.js
@@ -70,3 +70,25 @@ var obj3 = {
 if (obj3.bool) {
     console.log('Yo!');
 }
+
+// var sum = obj3.sum;
+// var total
+
+// parsing data
+var data = [
+    'Sianna','Fantini','sfantini0@jalbum.net','Female','17.187.75.7',
+    'Ami','Siggin','asiggin1@auda.org.au','Female','92.136.232.25',
+    'Kandy','Bernardoni','kbernardoni2@infoseek.co.jp','Female','108.122.66.8',
+    'Gus','Oswal','goswal3@ucoz.ru','Female','101.216.78.8',
+    'Laurie','Upward','lupward4@issuu.com','Male','92.123.107.',
+    'Melitta','Vallery','mvallery5@nydailynews.com','Female','255.228.200.8',
+    'Francesco','Danielczyk','fdanielczyk6@yelp.com','Male','253.12.207.4',
+    'Tedmund','Misken','tmisken7@imgur.com','Male','216.241.133.',
+    'Koralle','Griffithe','kgriffithe8@simplemachines.org','Female','108.21.21.11',
+    'Major','Yewman','myewman9@prnewswire.com','Male','142.167.101.22',
+    'Dell','Hutcheon','dhutcheona@forbes.com','Male','97.208.109.17',
+    'Halley','Doward','hdowardb@redcross.org','Female','58.97.154.14',
+    'Kati','Yakushkev','kyakushkevc@washington.edu','Female','34.166.161.14',
+    'Sargent','Hulatt','shulattd@w3.org','Male','99.235.176.11',
+    'Aleta','Tenny','atennye@rediff.com','Female','224.48.225.77' 
+    ];
